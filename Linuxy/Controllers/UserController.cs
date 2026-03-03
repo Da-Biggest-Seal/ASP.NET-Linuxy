fbba7e616336306cf57f471d79aad31153eaa052
Alexander Hamal <hamala24@spstrutnov.cz>
2026-03-03 10:49:57 +0100

    už opravdu opraveno

--- Linuxy/Controllers/UserController.cs
+++ Linuxy/Controllers/UserController.cs
@@ -6,5 +6,24 @@ namespace Linuxy.Controllers;
 
 public class UserController : Controller
 {
+    public IActionResult Login()
+    {
+        return View();
+    }
     
+    public IActionResult Signin()
+    {
+        return View();
+    }
+    
+    public IActionResult AfterLogin()
+    {
+        return View();
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
